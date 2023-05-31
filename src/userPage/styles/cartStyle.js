52dce062cbc19b91251c2d82d03ddc8b948bab65
alan264 <kylan3733@gmail.com>
2023-05-31 19:53:03 +0700

    fix

--- src/userPage/styles/cartStyle.js
+++ src/userPage/styles/cartStyle.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 export const RightStyle = styled.div`
   background-color:  #DABAAD;
     border-radius: 20px;
+  .tvc{
+    float: right;
+  }
 `;
 export const ContentStyle = styled.div`
     border-radius: 10px;
@@ -15,6 +18,9 @@ export const ContentStyle = styled.div`
 export const LeftStyle = styled.div`
   border: 1px solid rgb(192, 187, 187);
     border-radius: 20px;
+    .quanty{
+      padding:0 5px;
+    }
 `;
 export const SelectStyle = styled.select`
     width:50%;
