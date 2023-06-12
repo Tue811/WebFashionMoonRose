e199004d1b9fc44b7622a04d0e8f035abba30af9
alan264 <kylan3733@gmail.com>
2023-06-12 23:25:13 +0700

    update blog

--- src/userPage/components/Blog.jsx
+++ src/userPage/components/Blog.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   NewpostText,
   NewpostLeft,
@@ -12,9 +12,24 @@ import {
 } from "../styles/blogStyle";
 import { Button } from "antd";
 import { NavLink } from "react-router-dom";
-import { CalendarOutlined, FieldTimeOutlined } from "@ant-design/icons";
 
-const Blog = () => {
+import { CalendarOutlined, FieldTimeOutlined } from "@ant-design/icons";
+import UserContext from "../context/UserContext";
+import { listBlog } from "../services/productAction";
+
+const Blog = (props) => {
+  const { state, dispatch } = React.useContext(UserContext);
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    refresh();
+  }, []);
+
+  const refresh = async () => {
+    const t = await listBlog();
+
+    setBlogs(t);
+  };
   return (
     <section>
       <ContainerStyle>
@@ -28,29 +43,35 @@ const Blog = () => {
                 </NewpostText>
               </div>
             </div>
-            <div className="row">
-              <ImgLeftStyle className="col-6 ">
-                <div className="card">
-                  {/* <NewpostLeft img src="https://fashionjackson.com/wp-content/uploads/2021/03/Featured-Image-5.jpg" /> */}
-                  <NewpostLeft
-                    img
-                    src="https://images.unsplash.com/photo-1543163521-1bf539c55dd2?ixlib=rb-1.2.1&amp;q=70&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=768&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjExNzczfQ"
-                  />
-                </div>
-              </ImgLeftStyle>
-              <TitleRightStyle className="col-6">
-                Ngày đăng :<span>28 tháng 4 năm 2023</span>
-                <h1 className="mb-5 mt-3">
-                  Nhìn cuộc sống với đôi mắt của một đứa trẻ
-                </h1>
-                <NavLink to={"/blogdetail"}>
-                  <Button type="text" > Xem bài viết</Button>
-                </NavLink>
-                <span> 3 phút đọc</span>
-              </TitleRightStyle>
-            </div>
+            {blogs.map((v, k) => {
+              // console.log(v);
+              return(
+                <div className="row">
+                <ImgLeftStyle className="col-6 ">
+                  <div className="card">
+                    <NewpostLeft
+                      img
+                      src={v.avatar}
+                      // src="https://images.unsplash.com/photo-1543163521-1bf539c55dd2?ixlib=rb-1.2.1&amp;q=70&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=768&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjExNzczfQ"
+                    />
+                    
+                  </div>
+            
+                </ImgLeftStyle>
+                <TitleRightStyle className="col-6">
+                  Ngày đăng :<span>{v.date}</span>
+                  <h1 className="mb-5 mt-3">{v.title}</h1>
+                  <NavLink to={"/blogdetail"}>
+                    <Button type="text"> Xem bài viết</Button>
+                  </NavLink>
+                  <span> 3 phút đọc</span>
+                </TitleRightStyle>
+              </div>
+              )
+              
+            })}
 
-            <div className="row">
+            {/* <div className="row">
               <TitleLeftStyle className="col-6">
                 Ngày đăng :<span>28 tháng 4 năm 2023</span>
                 <h1 className="mb-5 mt-3">Một thương hiệu là một giọng nói</h1>
@@ -59,26 +80,23 @@ const Blog = () => {
                   quay lại. Tiến bộ để dạy anh ấy, chúng tôi rất đáng ngưỡng mộ.
                 </p>
                 <NavLink to={"/blogdetail"}>
-                  <Button type="text" > Xem bài viết</Button>
+                  <Button type="text"> Xem bài viết</Button>
                 </NavLink>
                 <span> 9 phút đọc</span>
               </TitleLeftStyle>
               <ImgRightStyle className="col-6 ">
                 <div className="card">
-                  {/* <NewpostLeft img src="https://fashionjackson.com/wp-content/uploads/2021/03/Featured-Image-5.jpg" /> */}
                   <NewpostLeft
                     img
                     src="https://images.unsplash.com/photo-1526887520775-4b14b8aed897?ixlib=rb-1.2.1&q=70&fm=jpg&crop=entropy&cs=tinysrgb&w=768&fit=max&ixid=eyJhcHBfaWQiOjExNzczfQ"
                   />
                 </div>
               </ImgRightStyle>
-            </div>
-
-            {/* ======================================== */}
+            </div> 
+            {/* ======================================== 
             <div className="row">
               <ImgLeftStyle className="col-6 ">
                 <div className="card">
-                  {/* <NewpostLeft img src="https://fashionjackson.com/wp-content/uploads/2021/03/Featured-Image-5.jpg" /> */}
                   <NewpostLeft
                     img
                     src="https://img.freepik.com/free-photo/sensual-young-stylish-sexy-woman-pink-luxury-dress-summer-fashion-trend-chic-style-blue-studio-background-holding-trendy-handbag_285396-2929.jpg?w=2000"
@@ -95,7 +113,6 @@ const Blog = () => {
                 <span> 3 phút đọc</span>
               </TitleRightStyle>
             </div>
-
             <div className="row">
               <TitleLeftStyle className="col-6">
                 <h1 className="mb-5">Một thương hiệu là một giọng nói</h1>
@@ -104,14 +121,13 @@ const Blog = () => {
               </TitleLeftStyle>
               <ImgRightStyle className="col-6 ">
                 <div className="card">
-                  {/* <NewpostLeft img src="https://fashionjackson.com/wp-content/uploads/2021/03/Featured-Image-5.jpg" /> */}
                   <NewpostLeft
                     img
                     src="https://images.unsplash.com/photo-1526887520775-4b14b8aed897?ixlib=rb-1.2.1&q=70&fm=jpg&crop=entropy&cs=tinysrgb&w=768&fit=max&ixid=eyJhcHBfaWQiOjExNzczfQ"
                   />
                 </div>
               </ImgRightStyle>
-            </div>
+            </div> */}
           </div>
         </div>
       </ContainerStyle>
