7deb1a911eeff71148d718ed8e1ba8da62693dd4
Nguyễn Thị Bích Tuệ <95497729+Tue811@users.noreply.github.com>
2023-06-14 03:19:10 +0700

    fix-list-products

--- src/userPage/pages/Products.jsx
+++ src/userPage/pages/Products.jsx
@@ -1,67 +1,70 @@
-import React ,{useState,useEffect}from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import { Col, Row } from 'antd';
-import CartProduct from '../components/CardProduct';
-import { listProducts, listCategories, addCart } from "../services/productAction";
-import UserContext from '../context/UserContext'
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { Col, Row,Checkbox  } from "antd";
+import CartProduct from "../components/CardProduct";
+import {
+  listProducts,
+  listCategories,
+  addCart,
+} from "../services/productAction";
+import UserContext from "../context/UserContext";
 
 const Products = () => {
-    const [products, setProducts]=useState([]);
-    const [categories, setCategories] = useState([])
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    useEffect(()=>{
-        refresh();
-        getListCategories()
-      },[])
-      
-      const refresh=async()=>{
-        const t=await listProducts();
-      
-        setProducts(t)
-      
-      
-      }
-      
-      const getListCategories =async()=>{
-        const c = await listCategories();
-        setCategories(c)
-      }
- 
-    return (
-        <div>
-          
-            <Row>
-                <Col span={6}>
-                 
+  useEffect(() => {
+    refresh();
+    getListCategories();
+  }, []);
 
-                </Col>
-                <Col span={18}>
-                    <Row>
-                    {
-               products.map((e,k) => {
-                return(
-                  <Col key={k} span={8}>
-            
-                  <CartProduct 
-                   product={e}
-                //   name={e.name}
-                //   price={e.finalprice}
-                //   src={e.thumbnail}
-                //   addToCart={()=>addToCart(e,k)}
-                //   addToFavourite={()=>addToFavourite(e,k)}
-                  />
-                
-                </Col>
-                )
-              })
-             }
-                    </Row>
-                </Col>
-            </Row>
-           
-        </div>
-    );
+  const refresh = async () => {
+    const t = await listProducts();
+
+    setProducts(t);
+  };
+
+  const getListCategories = async () => {
+    const c = await listCategories();
+    setCategories(c);
+  };
+
+  const nameCategories= categories.map((e)=>{
+    return e.name
+  })
+
+  console.log(nameCategories);
+
+  const onChange = (checkedValues) => {
+    console.log('checked = ', checkedValues);
+  };
+
+  return (
+    <div style={{padding:"0 50px 100px 50px"}}>
+
+            {categories.map((v) => {
+              return (
+                <div key={v.name}>
+                  <h4 style={{paddingTop:30}}>{v.name}</h4>
+                  <Row>
+                    {products.map((e, k) => {
+                      if (e.type === v.name) {
+                        return (
+                          <Col key={k} span={6}>
+                            <CartProduct product={e} />
+                          </Col>
+                        );
+                      }
+                      return null; // Add this line to handle the cases where the product doesn't match the category
+                    })}
+                  </Row>
+                </div>
+              );
+            })}
+
+    </div>
+  );
 };
 
 export default Products;
